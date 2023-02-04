75d7a16f415d9b9ab9163efc38219ee573d1171a
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-02-04 17:17:10 -0300

    add new route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { ColorInterpolate } from '../pages/ColorInterpolate';
 import { Home } from '../pages/Home';
 
 const Drawer = createDrawerNavigator();
@@ -7,6 +8,7 @@ export function Routes(): JSX.Element {
   return (
     <Drawer.Navigator>
       <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Color Interporlation" component={ColorInterpolate} />
     </Drawer.Navigator>
   );
 }
