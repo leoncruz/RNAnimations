de36f240ea72662e95aa0582c7e3100147862a61
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-02-04 15:45:47 -0300

    setup react navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { Text } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export function App(): JSX.Element {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Text>Hello World</Text>
+      <NavigationContainer>
+        <Text>Hello World</Text>
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 }
